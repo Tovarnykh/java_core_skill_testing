a79f03831930b3843abb041ade60a6c1e62ff2d2
Viktor <milaslavska2@gmail.com>
2022-03-15 11:03:50 +0100

    smth

--- Project/src/Polymorphism_practice/Company.java
+++ Project/src/Polymorphism_practice/Company.java
@@ -48,6 +48,8 @@ public class Company {
 	        } 
 	}
 	
-	
+	void loadWorkersFromFile() {
+		
+	}
 
 }
--- Project/src/Tests/Cat.java
+++ Project/src/Tests/Cat.java
@@ -12,4 +12,28 @@ public class Cat extends Animal{
 	public void setAdnDisplay() {
 		System.out.println(getName()+"fff");
 	}
+	
+	class Person{
+	     
+	    private String name;
+	    Account account;
+	 
+	    Person(String name, String password){
+	        this.name = name;
+	        account = new Account(password);
+	    }
+	    public void displayPerson(){
+	        System.out.printf("Person \t Name: %s \t Password: %s \n", name, account.password);
+	    }
+	}
+	public class Account{
+        private String password;
+         
+        Account(String pass){
+            this.password = pass;
+        }
+        void displayAccount(){
+            System.out.printf("Account Login: %s \t Password: %s \n", password);
+        }
+    }   
 }
