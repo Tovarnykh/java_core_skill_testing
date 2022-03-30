622c0c9eae209f075a5d4b876dc93f9a2a02027c
Viktor <milaslavska2@gmail.com>
2022-03-30 17:18:12 +0200

    New Gitignore

--- Project/src/Tests/Program.java
+++ Project/src/Tests/Program.java
@@ -1,42 +1,14 @@
 package Tests;
 
 public class Program {
-	
-	
+
 	public static void main(String[] args){
-		/*Animal cat = new Animal("Lucy");
-		((Cat)cat).display();*/
-		try {
-            boolean problem = true;
-            
-
-            Integer.parseInt("2932a"); // NumberFormatException
-
-            Object obj = new Object();
-            obj = null;
-            String str = obj.toString(); // NullPointerException
-            System.out.println(str);
-
-
-            System.out.println("Программа отработала");
-
-        } catch (NullPointerException e) {
-            System.out.println("Мы словили ошибку !! ура :)");
-            // 
-        } catch (NumberFormatException e) {
-            System.out.println("Текст введен не верно!!! :(");
-            e.printStackTrace();
-        } 
-
-        // программа продолжает работу
-        System.out.println("программа не крэшнулась и теперь выключается");
-    }
-	
-	
-	
+		
+	}
 	
 	private static void changeName(Animal p) {
-		p = new Animal("Jucy");
+		p = new Animal("Jucy", 124);
 		p.setName("Ju");
 	}
+	
 }
